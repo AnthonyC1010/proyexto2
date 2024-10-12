30e9502048cdde605126cb9bb8d7410c3aca2dd2
AnthonyC1010 <acarreral1@miumg.edu.gt>
2024-10-11 22:09:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ANTHONY BANCO - Inicio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <img src="logo.png" alt="Logo Anthony Banco" id="logo">
+        <h1>ANTHONY BANCO</h1>
+    </header>
+    
+
+    <nav>
+        <ul>
+            <li><a href="login_admin.php">Inicio de sesión de administrador</a></li>
+            <li><a href="login_cajero.php">Inicio de sesión de cajero</a></li>
+            <li><a href="login_usuario.php">Inicio de sesión de usuario</a></li>
+            <li><a href="gestion_usuario.php">Registro de nuevo usuario</a></li>
+        </ul>
+    </nav>
+
+        <p>&copy; 2024 ANTHONY BANCO. Todos los derechos reservados.</p>
+
+</body>
+</html>
